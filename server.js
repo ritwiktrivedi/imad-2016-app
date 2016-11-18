8eb51f5e222177eeaf629afedc8a553ac6908452
ritwiktrivedi <ritwik.trivedi@gmail.com>
2016-11-19 03:19:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -117,9 +117,20 @@ app.get('/submit-name', function(req,res) { // /submit-name?name=xxxx
 });
 
 
-app.get('/:articleName', function (req, res) {
-    var articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName])); 
+app.get('/articles/:articleName', function (req, res) {
+    
+    pool.query("SELECT * FROM article where title = '" + req.params.articleName + "'", function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length === 0) {
+                res.status(404).send('Article not found');
+            } else {
+                var articleData = result.rows[0];
+                res.send(createTemplate(articles[articleName])); 
+            }
+        }
+    });
 });
 
 app.get('/ui/style.css', function (req, res) {
