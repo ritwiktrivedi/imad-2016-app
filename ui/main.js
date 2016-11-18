ee42cd2fa8f1493bc73307a882c0a51c9a497d38
ritwiktrivedi <ritwik.trivedi@gmail.com>
2016-11-18 18:27:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,29 @@ function moveRight () {
 img.onclick = function() {
 	 var interval = setInterval(moveRight,50);
 };
+
+// Counter Code
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function () {
+	//Create a request
+	var request = new XMLHttpRequest();
+
+	//Capture the response and store it in a variable
+	request.onreadystatechange = function () {
+		if(request.readyState === XMLHttpRequest.DONE) {
+			// Take some action
+			if(request.status === 200) {
+				var counter = request.responseText;
+				var span = document.getElementById('count');
+				span.innerHTML = counter.toString();
+			}
+		}
+		//Not done yet
+
+	};
+	//Make the request
+	request.open('GET','http://ritwiktrivedi.imad.hasura-app.io', true);
+	request.send(null);
+};
