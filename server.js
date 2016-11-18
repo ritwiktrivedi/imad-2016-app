bb67195c5eae1e4fac6833f792578ece006db589
ritwiktrivedi <ritwik.trivedi@gmail.com>
2016-11-19 02:38:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,19 +10,6 @@ var config = {
     port: '5432',
     password: process.env.DB_PASSWORD
 };
-var pool = new Pool(config);
-app.get('test-db', function (req,res) {
-    // make a select request
-    // return a response with the results
-    pool.query('SELECT * FROM article', function (err, result) {
-        if (err) {
-            res.status(500).send(err.toString());
-        } else {
-            res.send(JSON.stringify(result));
-        }
-    });
-});
-
 
 var app = express();
 app.use(morgan('combined'));
@@ -100,7 +87,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var pool = new Pool(config);
+app.get('test-db', function (req,res) {
+    // make a select request
+    // return a response with the results
+    pool.query('SELECT * FROM article', function (err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }
+    });
+});
 
 var counter = 0;
 app.get('/counter', function(req, res) {
