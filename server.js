7735579e784e54fabda7a5776be8d6832eb63588
ritwiktrivedi <ritwik.trivedi@gmail.com>
2016-11-19 02:34:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ var pool = new Pool(config);
 app.get('test-db', function (req,res) {
     // make a select request
     // return a response with the results
-    pool.query('SELECT * FROM ARTICLE', function (err, result) {
+    pool.query('SELECT * FROM article', function (err, result) {
         if (err) {
             res.status(500).send(err.toString());
         } else {
