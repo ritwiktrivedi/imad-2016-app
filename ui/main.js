470c35d4ece631f5668cc650d7fb430458a8b94d
ritwiktrivedi <ritwik.trivedi@gmail.com>
2016-11-18 20:50:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,8 +38,6 @@ button.onclick = function () {
 	request.send(null);
 };
 // Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
 	//Create a request
@@ -65,6 +63,8 @@ submit.onclick = function () {
 
 	};
 	//Make the request
+	var nameInput = document.getElementById('name');
+    var name = nameInput.value;
 	request.open('GET','http://ritwiktrivedi.imad.hasura-app.io/submit-name?name=' + name, true);
 	request.send(null);
 };
