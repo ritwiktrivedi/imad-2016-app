a8f198e6c6fe4e600e656b18080f0b00572dbae3
ritwiktrivedi <ritwik.trivedi@gmail.com>
2016-11-19 17:51:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,43 +1,5 @@
-console.log('Loaded!');
 
-// Move the image
-
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-	marginLeft = marginLeft + 1;
-	img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function() {
-	 var interval = setInterval(moveRight,50);
-};
-
-// Counter Code
-var button = document.getElementById('counter');
-var counter = 0;
-
-button.onclick = function () {
-	//Create a request
-	var request = new XMLHttpRequest();
-
-	//Capture the response and store it in a variable
-	request.onreadystatechange = function () {
-		if(request.readyState === XMLHttpRequest.DONE) {
-			// Take some action
-			if(request.status === 200) {
-				var counter = request.responseText;
-				var span = document.getElementById('count');
-				span.innerHTML = counter.toString();
-			}
-		}
-		//Not done yet
-
-	};
-	//Make the request
-	request.open('GET','http://ritwiktrivedi.imad.hasura-app.io/counter', true);
-	request.send(null);
-};
-// Submit name
+// Submit username/password to login
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
 	//Create a request
@@ -48,23 +10,23 @@ submit.onclick = function () {
 		if(request.readyState === XMLHttpRequest.DONE) {
 			// Take some action
 			if(request.status === 200) {
-				//Capture a list of names and render it as a list
-				var names = request.responseText;
-				names = JSON.parse(names);
-				var list = "";
-				for(var i=0; i< names.length; i++) {
-				list += '<li>' + names[i] + '<li>';
-				}
-			var ul = document.getElementById('namelist');
-			ul.innerHTML = list;
+				console.log('user logged in');
+				alert('Logged in successfully');
+			} else if (request.status === 403) {
+			    alert('Username/password is incorrect');
+			} else if (request.status === 500) {
+			    alert('Something went wrong on the server');
 			}
 		}
 		//Not done yet
 
 	};
 	//Make the request
-	var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-	request.open('GET','http://ritwiktrivedi.imad.hasura-app.io/submit-name?name=' + name, true);
-	request.send(null);
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+	console.log(username);
+	console.log(password);
+    request.open('POST','http://ritwiktrivedi.imad.hasura-app.io/login', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+	request.send(JSON.stringify({username: username, password: password}));
 };
